b2fba0e13c87f343eab4b3bc128e0453a3f7c9b7
Thiago M. Grabe <tmgmeireles@gmail.com>
2026-07-06 11:47:22 -0300

    feat: adding .gitignore

--- sandbox_test/summarizer_service/exceptions.py
+++
@@ -1,8 +0,0 @@
-class PayloadTooLargeError(Exception):
-    """Raised when the input text exceeds the configured character cap."""
-
-    input_length: int = 0
-
-
-class SummarizerError(Exception):
-    """Raised when the upstream OpenAI call fails or times out."""
